c8e3ee26c8317da2f6986fe8fd0597773a99b389
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 16:51:16 +0300

    Revert "probe(webgoat): locate misses in eight crypto and access rows"
    This reverts commit 4f191b7177e515beb68811cf693c70ce38a70cc0.
    
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/idor/IDORViewOtherProfile.java
+++ src/main/java/org/owasp/webgoat/lessons/idor/IDORViewOtherProfile.java
@@ -46,6 +46,9 @@ public class IDORViewOtherProfile implements AssignmentEndpoint {
     if (obj != null && obj.equals("tom")) {
       // going to use session auth to view this one
       String authUserId = (String) userSessionData.getValue("idor-authenticated-user-id");
+      if (userId == null || !userId.equals(authUserId)) {
+        return failed(this).feedback("idor.view.profile.close2").build();
+      }
       if (userId != null && !userId.equals(authUserId)) {
         // on the right track
         UserProfile requestedProfile = new UserProfile(userId);
@@ -53,7 +56,7 @@ public class IDORViewOtherProfile implements AssignmentEndpoint {
         // the requested profile
         if (requestedProfile.getUserId() != null
             && requestedProfile.getUserId().equals("2342388")) {
-          return success(this)
+          return failed(this)
               .feedback("idor.view.profile.success")
               .output(requestedProfile.profileToMap().toString())
               .build();
