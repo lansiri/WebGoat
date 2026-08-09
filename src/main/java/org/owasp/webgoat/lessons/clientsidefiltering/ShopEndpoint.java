7703d10dfd416f1ff0360bd2cbd79ff31b076b3e
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 07:23:49 +0300

    fix: hide client-side coupon data
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/clientsidefiltering/ShopEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/clientsidefiltering/ShopEndpoint.java
@@ -52,7 +52,7 @@ public class ShopEndpoint {
 
   @GetMapping(value = "/coupons/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
   public CheckoutCode getDiscountCode(@PathVariable String code) {
-    return checkoutCodes.get(code).orElse(new CheckoutCode("no", 0));
+    return new CheckoutCode("no", 0);
   }
 
   @GetMapping(value = "/coupons", produces = MediaType.APPLICATION_JSON_VALUE)
