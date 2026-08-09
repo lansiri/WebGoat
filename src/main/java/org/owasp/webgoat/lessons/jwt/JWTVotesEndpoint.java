7cf8ed42ecd5849119e8c823005a905ca304d594
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 09:55:36 +0300

    Restore JWT Votes successful admin-reset outcome
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/jwt/JWTVotesEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/jwt/JWTVotesEndpoint.java
@@ -193,7 +193,7 @@ public class JWTVotesEndpoint implements AssignmentEndpoint {
           return failed(this).feedback("jwt-only-admin").build();
         } else {
           votes.values().forEach(vote -> vote.reset());
-          return failed(this).feedback("jwt-invalid-token").build();
+          return success(this).build();
         }
       } catch (JwtException e) {
         return failed(this).feedback("jwt-invalid-token").output(e.toString()).build();
