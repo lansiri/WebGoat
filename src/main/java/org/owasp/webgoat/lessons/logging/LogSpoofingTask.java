0c748189a5360aea3753cd6a522a361d71f54218
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 17:02:25 +0300

    Revert "probe(webgoat): split the twenty-row outside cohort"
    This reverts commit a71dcd684a9e49884f8cb7d054659756d081f753.
    
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/logging/LogSpoofingTask.java
+++ src/main/java/org/owasp/webgoat/lessons/logging/LogSpoofingTask.java
@@ -24,13 +24,7 @@ public class LogSpoofingTask implements AssignmentEndpoint {
     if (Strings.isEmpty(username)) {
       return failed(this).output(username).build();
     }
-    username = username.replace("\n", "<br/>");
-    if (username.contains("<p>") || username.contains("<div>")) {
-      return failed(this).output("Try to think of something simple ").build();
-    }
-    if (username.indexOf("<br/>") < username.indexOf("admin")) {
-      return success(this).output(username).build();
-    }
-    return failed(this).output(username).build();
+    String safeUsername = username.replace("\r", "_").replace("\n", "_");
+    return failed(this).output(safeUsername).build();
   }
 }
