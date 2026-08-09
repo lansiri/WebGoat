27ced2c1090f2c04345b77d90bf8622f4ddc3cf7
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 07:20:21 +0300

    fix: restrict WebWolf landing traces
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/webwolf/requests/Requests.java
+++ src/main/java/org/owasp/webgoat/webwolf/requests/Requests.java
@@ -62,11 +62,12 @@ public class Requests {
     /* do not show certain traces to other users in a classroom setup */
     if (req.getUri().getPath().contains("/files") && !req.getUri().getPath().contains(username)) {
       allowed = false;
-    } else if (req.getUri().getPath().contains("/landing")
-        && req.getUri().getQuery() != null
-        && req.getUri().getQuery().contains("uniqueCode")
-        && !req.getUri().getQuery().contains(StringUtils.reverse(username))) {
-      allowed = false;
+    } else if (req.getUri().getPath().contains("/landing")) {
+      String query = req.getUri().getQuery();
+      allowed =
+          query != null
+              && query.contains("uniqueCode")
+              && query.contains(StringUtils.reverse(username));
     }
 
     return allowed;
