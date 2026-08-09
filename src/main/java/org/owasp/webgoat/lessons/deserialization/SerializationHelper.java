f6a58ef4b89622bec1c5d1b023c1af51679eb391
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 03:48:44 +0300

    fix: protect recovery and challenge secrets
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/deserialization/SerializationHelper.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/SerializationHelper.java
@@ -4,11 +4,9 @@
  */
 package org.owasp.webgoat.lessons.deserialization;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Base64;
@@ -18,11 +16,7 @@ public class SerializationHelper {
   private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
 
   public static Object fromString(String s) throws IOException, ClassNotFoundException {
-    byte[] data = Base64.getDecoder().decode(s);
-    ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
-    Object o = ois.readObject();
-    ois.close();
-    return o;
+    throw new IOException("Native Java deserialization is disabled");
   }
 
   public static String toString(Serializable o) throws IOException {
