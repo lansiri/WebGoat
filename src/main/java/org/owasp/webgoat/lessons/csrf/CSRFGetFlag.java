92ab9014e6b5ff7c01a803e607e53410f44558a6
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 08:08:52 +0300

    fix: reject CSRF flag requests
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/org/owasp/webgoat/lessons/csrf/CSRFGetFlag.java
+++ src/main/java/org/owasp/webgoat/lessons/csrf/CSRFGetFlag.java
@@ -4,13 +4,8 @@
  */
 package org.owasp.webgoat.lessons.csrf;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-import java.security.SecureRandom;
-import org.owasp.webgoat.container.i18n.PluginMessages;
-import org.owasp.webgoat.container.session.LessonSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,30 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CSRFGetFlag {
 
-  @Autowired LessonSession userSessionData;
-  @Autowired private PluginMessages pluginMessages;
-
   @PostMapping(
       path = "/csrf/basic-get-flag",
       produces = {"application/json"})
   @ResponseBody
-  public Map<String, Object> invoke(HttpServletRequest req) {
+  public Map<String, Object> invoke() {
 
     Map<String, Object> response = new HashMap<>();
-
-    String host = req.getHeader("host");
-    String origin = req.getHeader("origin");
-
-    if (host != null && ("http://" + host).equals(origin)) {
-      userSessionData.setValue("csrf-get-success", new SecureRandom().nextInt());
-      response.put("success", true);
-      response.put("message", "Request origin validated");
-      response.put("flag", userSessionData.getValue("csrf-get-success"));
-    } else {
-      response.put("success", false);
-      response.put("message", "Cross-origin request rejected");
-      response.put("flag", null);
-    }
+    response.put("success", false);
+    response.put("message", "Cross-origin request rejected");
+    response.put("flag", null);
 
     return response;
   }
