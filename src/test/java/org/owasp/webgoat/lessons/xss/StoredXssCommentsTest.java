e674eb04fef400a1f10d6f9975849478df9053ac
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 08:03:44 +0300

    Revert "fix: encode stored XSS seed comment"
    This reverts commit 4a2de70b883c32a56169901dd7d4cae9037427ce.
    
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/test/java/org/owasp/webgoat/lessons/xss/StoredXssCommentsTest.java
+++ src/test/java/org/owasp/webgoat/lessons/xss/StoredXssCommentsTest.java
@@ -4,7 +4,6 @@
  */
 package org.owasp.webgoat.lessons.xss;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,11 +42,29 @@ class StoredXssCommentsTest extends LessonTest {
     results.andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(false)));
   }
 
+  /* For the next two tests there is a comment seeded ...
+     comments.add(new Comment("secUriTy", DateTime.now().toString(fmt), "<script>console.warn('unit test me')</script>Comment for Unit Testing"));
+     ... the isEncoded method will remain commented out as it will fail (because WebGoat isn't supposed to be secure)
+  */
+
+  // Ensures it is vulnerable
   @Test
-  public void seededCommentIsEncoded() throws Exception {
+  public void isNotEncoded() throws Exception {
+    // do get to get comments after posting xss payload
     ResultActions taintedResults =
         mockMvc.perform(MockMvcRequestBuilders.get("/CrossSiteScriptingStored/stored-xss"));
     MvcResult mvcResult = taintedResults.andReturn();
-    assertThat(mvcResult.getResponse().getContentAsString()).doesNotContain("<script>");
+    assert (mvcResult.getResponse().getContentAsString().contains("<script>console.warn"));
   }
+
+  // Could be used to test an encoding solution ... commented out so build will pass. Uncommenting
+  // will fail build, but leaving in as positive Security Unit Test
+  //    @Test
+  //    public void isEncoded() throws Exception {
+  //        //do get to get comments after posting xss payload
+  //        ResultActions taintedResults =
+  // mockMvc.perform(MockMvcRequestBuilders.get("/CrossSiteScripting/stored-xss"));
+  //
+  // taintedResults.andExpect(jsonPath("$[0].text",CoreMatchers.is(CoreMatchers.containsString("&lt;scriptgt;"))));
+  //    }
 }
