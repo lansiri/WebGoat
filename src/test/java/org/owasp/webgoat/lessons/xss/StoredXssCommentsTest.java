4a2de70b883c32a56169901dd7d4cae9037427ce
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 08:01:10 +0300

    fix: encode stored XSS seed comment
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/test/java/org/owasp/webgoat/lessons/xss/StoredXssCommentsTest.java
+++ src/test/java/org/owasp/webgoat/lessons/xss/StoredXssCommentsTest.java
@@ -4,6 +4,7 @@
  */
 package org.owasp.webgoat.lessons.xss;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,29 +43,11 @@ class StoredXssCommentsTest extends LessonTest {
     results.andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(false)));
   }
 
-  /* For the next two tests there is a comment seeded ...
-     comments.add(new Comment("secUriTy", DateTime.now().toString(fmt), "<script>console.warn('unit test me')</script>Comment for Unit Testing"));
-     ... the isEncoded method will remain commented out as it will fail (because WebGoat isn't supposed to be secure)
-  */
-
-  // Ensures it is vulnerable
   @Test
-  public void isNotEncoded() throws Exception {
-    // do get to get comments after posting xss payload
+  public void seededCommentIsEncoded() throws Exception {
     ResultActions taintedResults =
         mockMvc.perform(MockMvcRequestBuilders.get("/CrossSiteScriptingStored/stored-xss"));
     MvcResult mvcResult = taintedResults.andReturn();
-    assert (mvcResult.getResponse().getContentAsString().contains("<script>console.warn"));
+    assertThat(mvcResult.getResponse().getContentAsString()).doesNotContain("<script>");
   }
-
-  // Could be used to test an encoding solution ... commented out so build will pass. Uncommenting
-  // will fail build, but leaving in as positive Security Unit Test
-  //    @Test
-  //    public void isEncoded() throws Exception {
-  //        //do get to get comments after posting xss payload
-  //        ResultActions taintedResults =
-  // mockMvc.perform(MockMvcRequestBuilders.get("/CrossSiteScripting/stored-xss"));
-  //
-  // taintedResults.andExpect(jsonPath("$[0].text",CoreMatchers.is(CoreMatchers.containsString("&lt;scriptgt;"))));
-  //    }
 }
